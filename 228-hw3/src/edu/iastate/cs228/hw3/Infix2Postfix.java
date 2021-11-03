ed21e904a0ca7bd10d1729cb2438f70a6f6d881f
sjdefran1 <sjdefran@iastate.edu>
2021-11-03 16:51:09 -0500

    Scanner set up to read input.txt

--- 228-hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
+++ 228-hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
@@ -4,6 +4,12 @@ package edu.iastate.cs228.hw3;
  * @author Sam DeFrancisco
  */
 
+
+import java.io.File;
+import java.util.Scanner;
+
+
+
 public class Infix2Postfix
 {
 
@@ -15,7 +21,16 @@ public class Infix2Postfix
 
 	public static void main(String[] args) 
 	{
-		System.out.print("Testing github");
+
+		try {
+			File input = new File("C:\\Users\\sjdef\\Desktop\\CODE\\COM228\\hw3\\coms228-hw3\\228-hw3\\src\\edu\\iastate\\cs228\\hw3\\input.txt");
+			Scanner scan = new Scanner(input);
+		} catch (Exception e) {
+
+		}
+		
+
+
 	}
 
 }
