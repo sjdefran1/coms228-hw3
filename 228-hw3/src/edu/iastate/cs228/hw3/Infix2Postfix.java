53d77b15098cb53bacb6a738ec39fa1da773635d
sjdefran1 <sjdefran@iastate.edu>
2021-11-03 17:34:50 -0500

    Parenthesis now functional to read input file, infix2postfix creates instance of parenthesis and tests it out

--- 228-hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
+++ 228-hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
@@ -1,36 +1,29 @@
+
 package edu.iastate.cs228.hw3;
+
+
 
 /**
  * @author Sam DeFrancisco
  */
 
-
-import java.io.File;
 import java.util.Scanner;
-
+import java.io.File;
 
 
 public class Infix2Postfix
 {
-
-	public Infix2Postfix()
-	{
-		// TODO Auto-generated constructor stub
-	}
-
-
-	public static void main(String[] args) 
-	{
-
+	public static void main(String[] args) {
 		try {
 			File input = new File("C:\\Users\\sjdef\\Desktop\\CODE\\COM228\\hw3\\coms228-hw3\\228-hw3\\src\\edu\\iastate\\cs228\\hw3\\input.txt");
 			Scanner scan = new Scanner(input);
+
+			Parenthesis parenth = new Parenthesis(input);
+			boolean validParenth = parenth.parenthMatch();
+			System.out.println(validParenth);
 		} catch (Exception e) {
 
 		}
-		
-
-
 	}
 
 }
--- 228-hw3/src/edu/iastate/cs228/hw3/Parenthesis.java
+++ 228-hw3/src/edu/iastate/cs228/hw3/Parenthesis.java
@@ -4,7 +4,90 @@ package edu.iastate.cs228.hw3;
  * @author Sam DeFrancisco
  */
 
-
+ import java.io.File;
+ import java.util.Stack;
+ import java.util.Scanner;
+/**
+ * This class acts as a parenthsis scanner matcher
+ * finds if () {} [] within the file are valid
+ */
 public class Parenthesis {
     
+    /**
+     * file to be scanned
+     */
+    File input; 
+
+    /**
+     * String consiting of only the () {} [] within the file
+     */
+    String inputString = "";
+
+    public Parenthesis(File input)
+    {
+        this.input = input;
+        createString();
+    }
+
+
+    public void createString()
+    {
+        
+        try 
+        {
+            Scanner scan = new Scanner(input);
+
+            while(scan.hasNext())
+            {
+                String curr = scan.next();
+                if(curr.equals("(") || curr.equals(")"))
+                {
+                    inputString += curr;
+                }
+            }
+
+            System.out.println(inputString);
+            
+        } 
+        catch (Exception e) 
+        {
+            System.out.print("exception in createString()");
+        }
+        
+
+    }
+
+    public boolean parenthMatch()
+    {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < inputString.length(); i++)
+        {
+            char curr = inputString.charAt(i);
+            if(curr == '(')
+            {
+                stack.push(curr);
+            }
+            else if(curr == ')')
+            {
+                try{
+                    char open = stack.pop();
+                    char close = inputString.charAt(i);
+                    if((open == '(' && close != ')'))
+                    {
+                        return false;
+                    }
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Invalid parenthesis");
+                    return false; // uneven amount 
+                }
+            }
+        }
+        
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
 }
