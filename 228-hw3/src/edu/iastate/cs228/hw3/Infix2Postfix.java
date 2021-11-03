f0a34f55634ef979954958ca50c335b8acbb24bd
sjdefran1 <sjdefran@iastate.edu>
2021-11-03 16:27:20 -0500

    show me your power gitignore

---
+++ 228-hw3/src/edu/iastate/cs228/hw3/Infix2Postfix.java
@@ -0,0 +1,21 @@
+package edu.iastate.cs228.hw3;
+
+/**
+ * @author Sam DeFrancisco
+ */
+
+public class Infix2Postfix
+{
+
+	public Infix2Postfix()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public static void main(String[] args) 
+	{
+		System.out.print("Testing github");
+	}
+
+}
