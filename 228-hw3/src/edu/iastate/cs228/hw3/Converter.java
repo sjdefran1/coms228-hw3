5ffbc48ca69c28b1b1a75e62e6a8268926947863
sjdefran1 <sjdefran@iastate.edu>
2021-11-04 22:58:01 -0500

    refactored Converter.java, changed converter() from one giant method to calling helpers, also added javadoc for converter

--- 228-hw3/src/edu/iastate/cs228/hw3/Converter.java
+++ 228-hw3/src/edu/iastate/cs228/hw3/Converter.java
@@ -34,13 +34,19 @@ public class Converter {
         expression = input;
         stack = new Stack<Character>();
         postfixExpression = "";
-        converter();
+        converter(); //starts process of converting given infix to postfix
     }
 
 
 
     /**
+     * Calls helper methods to convert infix input to postfix
      * 
+     * Goes through expression string calling the handler method for different types
+     * calls handleOperand() when we are dealing with operand
+     * calls handleOperator() when dealing with operator 
+     * calls handleParenth() when handling ()'s
+     * Emptys stack onto postfixExpression when done going through infix expression string
      */
     public void converter()
     {
@@ -49,8 +55,6 @@ public class Converter {
         {
             return;
         }
-
-
         //go through entire infix expression
         for(int i = 0; i < expression.length(); i++)
         {
@@ -59,83 +63,30 @@ public class Converter {
             //operand
             if(Character.isDigit(curr) || Character.isLetter(curr))
             {
-                postfixExpression += curr;
+                //postfixExpression += curr;
+                handleOperand(curr);
             }
             //operator
             else if(InfixExpression.isOperator(curr) || InfixExpression.isExoponent(curr))
             {
-                boolean prec = prec(curr, true);
-            
-                //empty stack just push operator
-                //need to pop and give to postexpression
-                //then check against next top
-                if(!prec)
-                {
-                    while(!prec)
-                    {
-                        if(stack.isEmpty())
-                        {
-                            break;
-                        }
-                        postfixExpression += stack.pop();
-                        prec = prec(curr, true);
-                    }
-                    stack.push(curr);
-                }
-                //currPrec > topPrec
-                else
-                {
-                    stack.push(curr);
-                }
-               
-
-                
+                handleOperator(curr);
             }
 
             //parenth
             else if(Parenthesis.isParenthesis(curr))
             {   
-                //opening parenth gets pushed onto stack
-                if(curr == '(')
-                {
-                    stack.push(curr);
-                }
-                //closing parenth, pop's until finds opening parenth
-                else
-                {   
-                    if(stack.isEmpty())
-                    {
-                        //missing opening parenth, shouldve been caught by InfixExpression
-                        //we shall see
-                        break;
-                    }
-
-                    char popped = stack.pop();
-                    while(popped != '(')
-                    {
-                        if(stack.isEmpty())
-                        {
-                        //missing opening parenth, shouldve been caught by InfixExpression
-                        //we shall see
-                        break;
-                        }
-                        else
-                        {
-                            postfixExpression += popped;
-                            popped = stack.pop();
-                        }
-                    }
-                    //found opening parenth, go to next iteration to discard of both w/out adding
-                }
+                handleParenth(curr);
             }
-
         }  
         
+        //empty rest of operators to end of string
         while(!stack.isEmpty())
         {
             postfixExpression += stack.pop();
         }
     }
+
+
 
     /**
      * Refer to table above class definiton, this method will return
@@ -148,8 +99,8 @@ public class Converter {
      * @param givenChar
      * @param fromInput
      * @return
-     * //return true if you should just push givenChar 
-     * //return false if you should pop stack, and push givenChar
+     * return true if you should just push givenChar 
+     * return false if you should pop stack, add pop() to postfix, then compare givenChar to next top
      * 
      */
     public boolean prec(char givenChar, boolean fromInput)
@@ -182,6 +133,8 @@ public class Converter {
 
 
     /**
+     * 
+     * Returns prec int of givenChar based on if its from input or not
      * 
      * @param givenChar
      * @param fromInput
@@ -229,7 +182,102 @@ public class Converter {
         return givenElementPrec;
     }
 
+    /**
+     * When we have an operand all we need to do is add it to output string
+     * 
+     * @param curr - refers to last read element of input infix expression
+     */
+    private void handleOperand(char curr)
+    {
+        postfixExpression += curr;
+    }
 
+
+    /**
+     * When we have an operator we use the prec() method to figure out what we need to do with curr
+     * if prec() returns false while our curr element doesn't have lower prec then top, we pop top and add it
+     * to postfix string
+     * 
+     * if currPrec > topPrec we just push it to top of stack
+     * 
+     * @param curr refers to last read element of input infix expression
+     */
+    private void handleOperator(char curr)
+    {
+        boolean prec = prec(curr, true);
+            
+                //empty stack just push operator
+                //need to pop and give to postexpression
+                //then check against next top
+                if(!prec)
+                {
+                    while(!prec)
+                    {
+                        if(stack.isEmpty())
+                        {
+                            break;
+                        }
+                        postfixExpression += stack.pop();
+                        prec = prec(curr, true);
+                    }
+                    stack.push(curr);
+                }
+                //currPrec > topPrec
+                else
+                {
+                    stack.push(curr);
+                }
+    }
+
+
+    /**
+     * Called when needed to handle parenthesis for subexpressions
+     * 
+     * If(opening parenth) -> push it onto stack
+     * 
+     * if(closing parenth) -> pop everything and add it to postfix string untill we find the matching
+     *  opening parenthesis
+     * 
+     * @param curr refers to last read element of input infix expression
+     */
+    private void handleParenth(char curr)
+    {
+        //opening parenth gets pushed onto stack
+        if(curr == '(')
+        {
+            stack.push(curr);
+        }
+        //closing parenth, pop's until finds opening parenth
+        else
+        {   
+            if(stack.isEmpty())
+            {
+                return;
+            }
+
+
+            char popped = stack.pop();
+            while(popped != '(')
+            {
+                if(stack.isEmpty())
+                {
+                //missing opening parenth, shouldve been caught by InfixExpression
+                //we shall see
+                break;
+                }
+                else
+                {
+                    postfixExpression += popped;
+                    popped = stack.pop();
+                }
+            }
+            //found opening parenth, go to next iteration to discard of both w/out adding
+        }
+    }
+
+    /**
+     * outputs postfix expression
+     */
     @Override
     public String toString()
     {
